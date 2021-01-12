23cc1b25940fd6bda2c7498ac09e7f0301ce774d
Simon <googlproxer@gmail.com>
2021-01-12 16:23:57 +1100

    Scoped publish.

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './calendar.model';
-export { CalendarComponent, CalendarModal, CalendarWeekComponent, MonthComponent, MonthPickerComponent } from './components';
-export { DEFAULT_CALENDAR_OPTIONS } from './services/calendar-options.provider';
-export { CalendarModule } from './calendar.module';
-export { CalendarController } from './calendar.controller';
